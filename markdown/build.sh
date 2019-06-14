926f544d37818622731fc8525a02840650b68c41
Michael Pedersen <m.pedersen@icelus.org>
2019-06-14 05:20:44 +0000

    Auto-remove dangling images after build. Change ENV to ARG

--- markdown/build.sh
+++ markdown/build.sh
@@ -3,3 +3,7 @@ docker build -f Dockerfile.pandoc -t mjppandoc .
 docker build -f Dockerfile.libreoffice -t mjplibreoffice .
 docker build -f Dockerfile.gpp -t mjpgpp .
 docker build -f Dockerfile -t mjpresume .
+if [ ! -z "$(docker images -f dangling=true -q)" ]; then
+    docker rmi $(docker images -f dangling=true -q)
+fi
+docker run -p 8080:80 mjpresume:latest
