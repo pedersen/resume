f0fca1449bda487e7ef522074ff2cb2aa68d1400
Michael Pedersen <m.pedersen@icelus.org>
2019-07-06 23:24:47 -0400

    Alternative formats building, ready to deploy to prod

--- markdown/build.sh
+++ markdown/build.sh
@@ -2,8 +2,7 @@
 docker build -f Dockerfile.pandoc -t mjppandoc .
 docker build -f Dockerfile.libreoffice -t mjplibreoffice .
 docker build -f Dockerfile.gpp -t mjpgpp .
-docker build -f Dockerfile -t mjpresume .
-if [ ! -z "$(docker images -f dangling=true -q)" ]; then
-    docker rmi $(docker images -f dangling=true -q)
-fi
-docker run -p 8080:80 mjpresume:latest
+docker build -f Dockerfile -t mjpresume:latest .
+#if [ ! -z "$(docker images -f dangling=true -q)" ]; then
+#    docker rmi $(docker images -f dangling=true -q)
+#fi
